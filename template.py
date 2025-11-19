3e141ae5f166cacb1b3f98cf3c3dc04d39718028
Dave Burke <dburke84@gmail.com>
2025-11-19 16:55:45 -0600

    Format with isort and black

--- template.py
+++ template.py
@@ -1,14 +1,16 @@
 #!/bin/env python3
 
-import aoc
 import logging
 
+import aoc
+
 logging.basicConfig(level=logging.WARN)
+
 
 def part1(input):
     print(input)
 
+
 if __name__ == "__main__":
     input = aoc.input(1)
     part1(input)
-
